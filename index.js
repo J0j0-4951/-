1a8312461d0940454cd7cc05acd56383daa2fe7f
J0j0-4951 <jhos3lin3s.z@gmail.com>
2025-06-17 03:50:01 +0000

    added more varibles

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 console.log('Hello, World!');
+
 let name= 'What?';
+
 console.log(name);
+
+let firstName= Jimmy
+let lastName= Nielson
+
+let interestRate =0.3;
+interestRate = 1;
+console.log(interestRate);
 
